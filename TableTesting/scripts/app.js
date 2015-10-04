7ad13f2549dc0f54aadfed323c5bfa9decf5e96e
FranLsz <franlsz95develop@gmail.com>
2015-10-04 15:00:02 +0200

    añadido estilo y animaciones

--- TableTesting/scripts/app.js
+++ TableTesting/scripts/app.js
@@ -1,13 +1,17 @@
 ﻿
-
-
 $(document).ready(function () {
+    //preset
+    $("#addNombre").val("Fran");
+    $("#addApellidos").val("López");
+
+    //row counter
     var i = 1;
+
     function add() {
         var n = $("#addNombre").val();
         var a = $("#addApellidos").val();
 
-        var row = "<tr><td>"+ i++ +"</td><td>" + n + "</td><td>" + a + "</td></tr>";
+        var row = "<div class='erow'><span>" + i++ + "</span><span>" + n + "</span><span>" + a + "</span></div>";
 
         $("#DataContentZone").append(row);
     }
