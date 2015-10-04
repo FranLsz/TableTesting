cc0ac8342e947b2ae9f21cb3ba17c5806e5236e5
FranLsz <franlsz95develop@gmail.com>
2015-10-04 13:06:10 +0200

    añadida funcionalidad basica

--- TableTesting/scripts/app.js
+++ TableTesting/scripts/app.js
@@ -1,1 +1,19 @@
 ﻿
+
+
+$(document).ready(function () {
+    var i = 1;
+    function add() {
+        var n = $("#addNombre").val();
+        var a = $("#addApellidos").val();
+
+        var row = "<tr><td>"+ i++ +"</td><td>" + n + "</td><td>" + a + "</td></tr>";
+
+        $("#DataContentZone").append(row);
+    }
+
+
+    $("#btnAdd").click(add);
+
+
+});
